20b7075a25a39aab68f4cd8e79136997c79acfe8
Aleksandr <barryalien30716@gmail.com>
2025-11-07 23:20:23 +0700

    Initial Commit

---
+++ MonoArtTests/MonoArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonoArtTests.swift
+//  MonoArtTests
+//
+//  Created by Александр Ращектаев on 07.11.2025.
+//
+
+import Testing
+@testable import MonoArt
+
+struct MonoArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MonoArtUITests/MonoArtUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MonoArtUITestsLaunchTests.swift
+//  MonoArtUITests
+//
+//  Created by Александр Ращектаев on 07.11.2025.
+//
+
+import XCTest
+
+final class MonoArtUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
